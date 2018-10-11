32374f36064f2bab88f59cacfcfb72d6f62a617c
Megan Farley <megfarley4@gmail.com>
2018-10-11 12:21:15 -0500

    changes for program

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
+require_relative './fish.rb'
+require_relative './cat.rb'
+require_relative './dog.rb'
+
 class Owner
-  # code goes here
+
+  @@all = []
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  def initialize(name)
+    self.name = name
+    @species = "human"
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def buy_fish(fish_name)
+    pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each do |key, value|
+      value.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
 end
+
